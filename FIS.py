527b3f4c6548b9ca78948d1999255feb88090648
SetupID <setupid107@gmail.com>
2020-01-13 08:50:25 +0700

    Film Searcher

--- FIS.py
+++ FIS.py
@@ -23,18 +23,18 @@ def golek(film):
 	header = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0"}
 	h2 = header.update({'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'})
 	req = requests.get(s,headers=header).text
-	scrap = bs(req,'lxml')
+	scrap = bs(req,'html.parser')
 	div = scrap.find('div',{'class': 'content-thumbnail text-center'})
 	link = div.a.get('href')
 	al = requests.get(link,headers=header).text #access link
-	scrp = bs(al,'lxml')
+	scrp = bs(al,'html.parser')
 	id = scrp.find('div',{'id':'muvipro_player_content_id'}).get('data-id')
 	d = {'action':'muvipro_player_content','tab':'player1','post_id':id}
 	pos = requests.post('https://kawanfilm21.online/wp-admin/admin-ajax.php',headers=h2,data=d).text
 	result['title'] = scrp.find('h1',{'class':'entry-title'}).get_text()
 	result['description'] = scrp.find_all('p')[1].get_text()
 	result['quality'] = scrp.find('span',{'class':'gmr-movie-quality'}).a.get_text()
-	stream = bs(pos,'lxml').iframe.get('src')
+	stream = bs(pos,'html.parser').iframe.get('src')
 	result['streaming'] = 'https:'+stream
 	gl = []  #get link
 	for l in scrp.find_all('ul',{'class':'list-inline gmr-download-list clearfix'}):
